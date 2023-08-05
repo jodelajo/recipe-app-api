1df9ebe10900d661519b86b1933060e0feecebca
jodelajo <jodelajo@gmail.com>
2023-08-05 13:21:55 +0200

    fix pipeline fail

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -40,7 +40,7 @@ class ModelTests(TestCase):
     def test_create_superuser(self):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
-            'test@example.com' ,
+            'test@example.com',
             'test123'
         )
 
